1cef09b31c9427568992f738e55a3fe08b2b003e
maria496958985 <78129429+maria496958985@users.noreply.github.com>
2021-01-28 07:28:26 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+
+    $('.photo__slaider').slick({
+       
+        prevArrow: '<button type="button" class="slick-prev"> <img src="img/strelka1.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"> <img src="img/strelka2.svg" alt=""></button>',
+    });
+
+});
